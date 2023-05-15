36a9540e2f2c12ae96f4888eaae99bb2930acabb
uczen <piotrlada123@interia.pl>
2023-05-15 12:46:23 +0200

    v1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsola2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Klasa 2P");
+            Console.WriteLine("Grupa 2");
+        }
+    }
+}
